b748aee443cf872386156d86afa43e3216276a74
Tyler Akin <takin963@students.sbts.edu>
2019-12-14 16:26:36 -0500

    small changes

--- src/PageContent.js
+++ src/PageContent.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, useLocation } from 'react-router-dom';
 import {useTransition, animated} from 'react-spring';
 
-import AboutPageContent from './Content/AboutPageContent';
 import HomePageContent from './Content/HomePageContent';
+import AboutPageContent from './Content/AboutPageContent';
 import Contact from './Content/Contact';
 import WorkContent from './Content/WorkContent';
 import Portfolio from './Content/Portfolio';
@@ -21,18 +21,17 @@ function PageContent() {
     leave: {  transform: 'translate(-50%,0)' },
   })
   return transitions.map(({ item: location, props, key }) => (
-     <div className="page-content-animation-wrapper">
-    <animated.div key={key} style={props}>
-      <Switch location={location}>
-      <Route exact path='/' component={HomePageContent} />
-      <Route exact path='/about-tyler' component={AboutPageContent} />
-      <Route exact path='/contact' component={Contact} />
-      <Route exact path='/work' component={WorkContent} />
-      <Route exact path='/work/portfolio' component={Portfolio} />
-      <Route exact path='/work/technology' component={Technology} />
-      </Switch>
-    </animated.div>
-
+     <div className="page-content-animation-wrapper" >
+         <animated.div key={key} style={props}>
+           <Switch location={location}>
+               <Route exact path='/about-tyler' render={() => <AboutPageContent /> } />
+               <Route exact path='/' render={() => <HomePageContent /> } />
+               <Route exact path='/contact' render={() => <Contact /> } />
+               <Route exact path='/work' render={() => <WorkContent /> } />
+               <Route exact path='/work/portfolio' render={() => <Portfolio /> } />
+               <Route exact path='/work/technology' render={() => <Technology /> } />
+           </Switch>
+         </animated.div>
       </div>
   ))
 
