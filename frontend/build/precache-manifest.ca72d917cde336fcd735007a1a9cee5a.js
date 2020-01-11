f927fc856052fec354ae97badb4439af43021c26
Tyler Akin <takin963@students.sbts.edu>
2020-01-10 22:56:25 -0500

    changes to set homepage to domain

---
+++ frontend/build/precache-manifest.ca72d917cde336fcd735007a1a9cee5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057dc58f4adbca635ec3e4f9599b8e8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a195db851fcaf994b261",
+    "url": "/static/css/main.55f081fd.chunk.css"
+  },
+  {
+    "revision": "f7f31a8785759ad4181d",
+    "url": "/static/js/2.f6aeb8aa.chunk.js"
+  },
+  {
+    "revision": "40aad3a8e714aaa8b071e4e33d516e64",
+    "url": "/static/js/2.f6aeb8aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a195db851fcaf994b261",
+    "url": "/static/js/main.2de2778b.chunk.js"
+  },
+  {
+    "revision": "b5ff61001688404db26f",
+    "url": "/static/js/runtime-main.8c5f780f.js"
+  },
+  {
+    "revision": "a1fd5dd4333bdd745b0fb1acd8b40090",
+    "url": "/static/media/double-arrow.a1fd5dd4.svg"
+  }
+]);
